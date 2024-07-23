8b8b6360d60e773bb4276f58e455930be87c386a
Aditia A. Pratama <aditia.ap@gmail.com>
2024-07-23 19:38:51 +0800

    add tool asset to create .lo asset

---
+++ tool/asset.py
@@ -0,0 +1,56 @@
+# assetpublisher
+# Copyright (C) 2024  Aditia A. Pratama
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import os
+import bpy
+from . import system
+
+
+class Asset:
+
+    @classmethod
+    def create_lo_asset(cls):
+        props = bpy.context.scene.APMetadataProperties
+        name = props.meta_asset_name
+        code = props.meta_asset_code
+        asset_name = f"{code}{name}_MDL.hi"
+        hi_collection = bpy.data.collections.get(asset_name)
+        hi_objects = [o for o in hi_collection.objects if o.type == "MESH"]
+        transparent_shader = bpy.data.materials.get("SHD_Transparent")
+        if not transparent_shader:
+            materials_path = system.System.get_addon_data("materials")
+            asset_path = os.path.join(materials_path, "shader.blend")
+            with bpy.data.libraries.load(asset_path, link=False) as (
+                data_from,
+                data_to,
+            ):
+                for mat in data_from.materials:
+                    if mat == "SHD_Transparent":
+                        data_to.materials.append(mat)
+            transparent_shader = bpy.data.materials.get("SHD_Transparent")
+
+        lo_collection = bpy.data.collections.get(f"{code}{name}_MDL.lo")
+        if not lo_collection.objects:
+            for obj in hi_objects:
+                lo_obj = system.System.duplicate(
+                    obj, data=False, actions=False, collection=lo_collection
+                )
+                if not lo_obj.name.endswith(".lo"):
+                    lo_obj.name = f"{lo_obj.name}.lo"
+
+                for slot in lo_obj.material_slots:
+                    slot.link = "OBJECT"
+                    slot.material = transparent_shader
